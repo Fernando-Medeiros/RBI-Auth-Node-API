fb80843debfda42922db353e86d14ce50fb7b512
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-02-08 16:22:59 -0300

    refactor(database): add export para testes e ajustes com o eslint

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,27 +1,25 @@
-const mongo = require("mongoose");
-mongo.set("strictQuery", false);
+import { set, connect } from "mongoose";
 
-const checkEnv = () => {
-  let environment = process.env.ENV || "dev";
-  let db = "";
+set("strictQuery", false);
 
-  switch (environment.toLowerCase()) {
-    case "pro":
-      db = process.env.MONGO_DATABASE;
-    case "dev":
-      db = process.env.MONGO_DEV_DATABASE;
-    case "tes":
-      db = process.env.MONGO_TEST_DATABASE;
-    default:
-      return {
-        username: process.env.MONGO_USERNAME,
-        password: process.env.MONGO_PASSWORD,
-        database: db,
-      };
+export const checkEnv = () => {
+  let _env = process.env.ENV || "dev";
+  let db = process.env.MONGO_DEV_DATABASE;
+
+  if (_env.toLowerCase() === "pro") {
+    db = process.env.MONGO_DATABASE;
+  } else if (_env.toLowerCase() === "test") {
+    db = process.env.MONGO_TEST_DATABASE;
   }
+
+  return {
+    username: process.env.MONGO_USERNAME,
+    password: process.env.MONGO_PASSWORD,
+    database: db,
+  };
 };
 
-const getUri = ({ username, password, dabatase }) => {
+export const getUri = ({ username, password, dabatase }) => {
   try {
     return `mongodb+srv://${username}:${password}.cy0lncg.mongodb.net/${dabatase}?retryWrites=true&w=majority`;
   } catch (error) {
@@ -29,13 +27,11 @@ const getUri = ({ username, password, dabatase }) => {
   }
 };
 
-const connectDatabase = async () => {
-  await mongo.connect(getUri(checkEnv()), (error) => {
+export const connectDatabase = async () => {
+  await connect(getUri(checkEnv()), (error) => {
     if (error) {
       return console.log(`Erro ao conectar com o bando de dados! ${error}`);
     }
     return console.log("Conectado ao banco de dados");
   });
 };
-
-module.exports = connectDatabase;
