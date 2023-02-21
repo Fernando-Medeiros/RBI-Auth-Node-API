e33d6a8e3a2024d21bb99603ab02971ad818c2ab
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-02-20 22:30:18 -0300

    feat: add http.Exceptions -> class -> errors

---
+++ src/helpers/http.exceptions.ts
@@ -0,0 +1,26 @@
+export class BaseException extends Error {
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export class BadRequest extends BaseException {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class NotFound extends BaseException {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+
+export class Unauthorized extends BaseException {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
