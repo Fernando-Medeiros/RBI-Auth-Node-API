7444173a3f406d2ec37a2d89c4fac07626560b86
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-02-25 20:39:45 -0300

    feat(docs): documentação das rotas utilizando swagger; add script para gerar a documentação

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
 import "express-async-errors";
 import express from "express";
+import { env } from "process";
+
+import { exceptionMiddleware } from "./middlewares/exceptions";
+
 import { route as authRoute } from "./resources/auth";
 import { route as customerRoute } from "./resources/customer";
-import { exceptionMiddleware } from "./middlewares/exceptions";
+
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./docs/swagger.json";
+
+const PORT = env["PORT"] ?? 8080;
 
 export const server = express();
 
-const PORT = process.env["PORT"] || 8080;
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 server.use(express.json());
+
 server.use(authRoute);
 server.use(customerRoute);
 
